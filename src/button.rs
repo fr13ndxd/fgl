b2955adc7a526cfefdead39b7d530d2f5e4d8370
fr13nd <fr13nd65@protonmail.com>
2024-07-19 14:09:52 +0200

    add button poll

---
+++ src/button.rs
@@ -0,0 +1,56 @@
+use futures::stream::StreamExt;
+use glib::interval_stream;
+use gtk4::glib;
+use gtk4::traits::ButtonExt;
+use gtk4::Button;
+use std::time::Duration;
+
+pub trait LabelPoll {
+    fn poll<F>(&self, interval: u64, func: F)
+    where
+        F: Fn() -> String + 'static;
+
+    fn watch<F>(&self, func: F)
+    where
+        F: Fn() -> String + Send + Sync + 'static;
+}
+
+impl LabelPoll for Button {
+    fn poll<F>(&self, interval: u64, func: F)
+    where
+        F: Fn() -> String + 'static,
+    {
+        let button = self.clone();
+        let stream = interval_stream(Duration::from_millis(interval));
+
+        glib::MainContext::default().spawn_local(async move {
+            let mut stream = stream;
+            while let Some(_) = stream.next().await {
+                let new_text = func();
+                button.set_label(&new_text);
+            }
+        });
+    }
+
+    fn watch<F>(&self, func: F)
+    where
+        F: Fn() -> String + Send + Sync + 'static,
+    {
+        let button = self.clone();
+        let stream = interval_stream(Duration::from_millis(100));
+
+        button.set_label(&func());
+
+        glib::MainContext::default().spawn_local(async move {
+            let mut stream = stream;
+            let mut old = func();
+            while let Some(_) = stream.next().await {
+                let new_text = func();
+                if new_text != old {
+                    button.set_label(&new_text);
+                    old = new_text;
+                }
+            }
+        });
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,3 @@
+pub mod button;
 pub mod label;
 pub mod widgets;
