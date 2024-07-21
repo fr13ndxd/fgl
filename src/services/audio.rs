4d815ea571e77f3632b11ff961786ac5acaa357c
fr13nd <fr13nd65@protonmail.com>
2024-07-21 20:21:44 +0200

    audio: add is_muted() and final fixes..

--- src/services/audio.rs
+++ src/services/audio.rs
@@ -1,15 +1,25 @@
 use std::process::Command;
 
+pub fn is_muted() -> bool {
+    let output = Command::new("pactl")
+        .args(["get-sink-mute", "@DEFAULT_SINK@"])
+        .output()
+        .unwrap();
+    let output = std::str::from_utf8(&output.stdout).unwrap();
+
+    output.contains("yes")
+}
+
 pub fn get_volume() -> i32 {
     let output = Command::new("pactl")
         .args(["get-sink-volume", "@DEFAULT_SINK@"])
         .output()
         .unwrap();
 
-    let output = String::from_utf8_lossy(output.stdout());
+    let output = std::str::from_utf8(&output.stdout).unwrap();
 
     for line in output.lines() {
-        if let Some(vol_str) = line.split_whitespace().nth(1) {
+        if let Some(vol_str) = line.split_whitespace().nth(4) {
             let vol = vol_str
                 .trim_end_matches('%')
                 .parse::<i32>()
@@ -18,16 +28,21 @@ pub fn get_volume() -> i32 {
         }
     }
 
-    -1
+    0
 }
 
 pub fn get_audio_icon() -> String {
+    if is_muted() {
+        return String::from("audio-volume-muted-symbolic");
+    }
+
     let volume = get_volume();
 
     return match volume {
-        ..0 => String::from("audio-volume-muted-symbolic"),
-        1..34 => String::from("audio-volume-low-symbolic"),
-        34..67 => String::from("audio-volume-high-symbolic"),
-        67.. => String::from("audio-volume-overamplified-symbolic"),
+        0 => String::from("audio-volume-muted-symbolic"),
+        1..=34 => String::from("audio-volume-low-symbolic"),
+        35..=67 => String::from("audio-volume-medium-symbolic"),
+        68..=100 => String::from("audio-volume-high-symbolic"),
+        _ => String::from("audio-volume-overamplified-symbolic"),
     };
 }
